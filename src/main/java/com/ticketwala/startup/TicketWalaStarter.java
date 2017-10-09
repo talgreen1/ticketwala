cb1f83b839bc763d7e52de024e09be35403dfdbe
Yotam Avivi <yavivi@Yotams-MacBook-Pro.local>
2017-10-09 21:47:32 +0300

    naive command parser

--- src/main/java/com/ticketwala/startup/TicketWalaStarter.java
+++ src/main/java/com/ticketwala/startup/TicketWalaStarter.java
@@ -1,9 +1,32 @@
 package com.ticketwala.startup;
 
+import java.util.Scanner;
+
 public class TicketWalaStarter {
 
 	public static void main(String[] args) {
-		System.out.println("TicketWala Application");
+		Scanner scanner = new Scanner(System.in);
+		
+		String userCommandLine = "";
+		
+		System.out.println("Enter your command:\n");
+		
+		while (!"bye".equals(userCommandLine)) {
+			try {
+				userCommandLine = scanner.nextLine();
+				String[] commandArray = userCommandLine.trim().split("\\s+"); //Split on white spaces of any size
+				
+				System.out.println("Command : " + commandArray[0]);
+				for (int i = 1; i < commandArray.length; i++) {
+					System.out.println("Arg" + i + " : " + commandArray[i]);
+				}
+			} catch (Exception e) {
+				System.err.println("Unexpected Command Line Error! " + e.getMessage());
+			}
+		}
+		
+		System.out.println("Bye...");
+		scanner.close();		
 	}
 
 }
