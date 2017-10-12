0d9a8b1be85ca4e8734aa98a9cf97c94852fc927
Yotam Avivi <yavivi@Yotams-MacBook-Pro.local>
2017-10-12 16:14:12 +0300

    TW Service & Empty Impl

---
+++ src/main/java/com/ticketwala/service/api/TicketWalaService.java
@@ -0,0 +1,66 @@
+package com.ticketwala.service.api;
+
+import java.util.List;
+
+import com.ticketwala.command.api.Result;
+import com.ticketwala.model.MovieShow;
+import com.ticketwala.model.Seat;
+
+public interface TicketWalaService {
+	
+	/**
+	 * Get the list of all existing movie shows
+	 * 
+	 * @return A List of MovieShow
+	 */
+	List<MovieShow> getMovieShows();
+
+	/**
+	 * Find and return a single movie show
+	 * 
+	 * @return MovieShow
+	 */
+	MovieShow getMovieShow(String showId);
+	
+	/**
+	 * Start an order for a Movie Show selected by the user.
+	 * 
+	 * @param showId
+	 * @return Result
+	 */
+	Result createOrder(String showId);
+	
+	/**
+	 * Add a seat ticket to an existing Order.
+	 * 
+	 * @param orderId
+	 * @return Result
+	 */
+	Result addSeatTicket(String orderId, Seat seat);
+	
+	/**
+	 * Submit the given Order.
+	 * 
+	 * @param orderId
+	 * @return Result
+	 */
+	Result submitOrder(String orderId);
+	
+	
+	/*************************************** Admin Operations ******************************************/
+	/**
+	 * Add a Movie Show to the System
+	 * 
+	 * @param movieShow
+	 * @return Result
+	 */
+	Result addMovieShow(MovieShow movieShow);
+	
+	/**
+	 * Delete a Movie Show from the System
+	 * 
+	 * @param showId
+	 * @return Result
+	 */
+	Result deleteMovieShow(String showId);
+}
---
+++ src/main/java/com/ticketwala/service/impl/TicketWalaServiceImpl.java
@@ -0,0 +1,55 @@
+package com.ticketwala.service.impl;
+
+import java.util.List;
+
+import com.ticketwala.command.api.Result;
+import com.ticketwala.model.MovieShow;
+import com.ticketwala.model.Seat;
+import com.ticketwala.service.api.TicketWalaService;
+
+public class TicketWalaServiceImpl implements TicketWalaService {
+	
+	@Override
+	public List<MovieShow> getMovieShows() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MovieShow getMovieShow(String showId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result createOrder(String showId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result addSeatTicket(String orderId, Seat seat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result submitOrder(String orderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Result addMovieShow(MovieShow movieShow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result deleteMovieShow(String showId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
