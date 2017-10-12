3eb67a577fd155b65b9967a5429394da137161cb
Yotam Avivi <yavivi@Yotams-MacBook-Pro.local>
2017-10-12 21:18:00 +0300

    data access service and empty impl

---
+++ src/main/java/com/ticketwala/dao/impl/DataAccessServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ticketwala.dao.impl;
+
+import java.util.HashMap;
+
+import com.ticketwala.command.api.Result;
+import com.ticketwala.dao.api.DataAccessService;
+import com.ticketwala.model.MovieShow;
+import com.ticketwala.model.Order;
+
+public class DataAccessServiceImpl implements DataAccessService {
+
+	@Override
+	public Result createMovieShow(MovieShow movieShow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MovieShow findMovieShow(String movieShowId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result deleteMovieShow(String movieShowId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result updateMovieShow(MovieShow movieShow) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashMap<String, MovieShow> getMovies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Result commitOrder(Order order) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
