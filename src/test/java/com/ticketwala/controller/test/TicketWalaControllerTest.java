a509dd6899c1c637bfbd84397aa0efad3dff1c97
Green, Tal (tg4920) <tg4920@intl.att.com>
2018-11-20 14:55:14 +0200

    Change tests to use MockMVC

--- src/test/java/com/ticketwala/controller/test/TicketWalaControllerTest.java
+++ src/test/java/com/ticketwala/controller/test/TicketWalaControllerTest.java
@@ -1,60 +1,76 @@
 package com.ticketwala.controller.test;
 
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
 
-import java.time.LocalDateTime;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ticketwala.TicketWalaWebApplication;
 import com.ticketwala.command.input.CreateMovieShowInput;
 import com.ticketwala.dao.api.DataAccessService;
 import com.ticketwala.dao.impl.DataAccessServiceImpl;
 import com.ticketwala.model.MovieShow;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(
-		  webEnvironment = WebEnvironment.DEFINED_PORT,
-		  classes = TicketWalaWebApplication.class)
+@SpringBootTest(classes = TicketWalaWebApplication.class)
+@AutoConfigureMockMvc
 public class TicketWalaControllerTest {
 
-	private void prepareDatabase() {
-		DataAccessService das = new DataAccessServiceImpl();
-		das.deleteAllMovieShows();
-		das.createMovieShow(new MovieShow("12345", "Star Wars III", LocalDateTime.now(), 120));
-		das.createMovieShow(new MovieShow("12346", "Star Wars V", LocalDateTime.now(), 120));
-	}
-	
-	@Test
-	public void testGetMovieShow(){
-		prepareDatabase();
-		get("/movie_shows/12345").
-		then().
-		body("id", equalTo("12345")).and().body("movieName", equalTo("Star Wars III"));
-	}
-	
-	@Test
-	public void testGetMovieShows(){
-		prepareDatabase();
-		get("/movie_shows").
-		then().body("id", hasItems("12346", "12345"));
-	}
-	
-	@Test
-	public void testCreateMovieShow() {
-		CreateMovieShowInput movieShow = new CreateMovieShowInput("Superman", 87, "2017-12-31T18:09");
-		prepareDatabase();
-		given().
-		contentType("application/json").
-		body(movieShow).
-		when().post("/movie_shows").then().assertThat().statusCode(201);
-	}
+    @Autowired
+    private MockMvc mockMvc;
+
+    private void prepareDatabase() {
+        DataAccessService das = new DataAccessServiceImpl();
+        das.deleteAllMovieShows();
+        das.createMovieShow(new MovieShow("12345", "Star Wars III", LocalDateTime.now(), 120));
+        das.createMovieShow(new MovieShow("12346", "Star Wars V", LocalDateTime.now(), 120));
+    }
+
+    @Test
+    public void testGetMovieShow() throws Exception {
+        prepareDatabase();
+
+        mockMvc.perform(get("/movie_shows/12345"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("id", equalTo("12345")))
+                .andExpect(jsonPath("movieName", equalTo("Star Wars III")));
+    }
+
+    @Test
+    public void testGetMovieShows() throws Exception {
+        prepareDatabase();
+
+        mockMvc.perform(get("/movie_shows"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(".id", hasItems("12346", "12345")));
+    }
+
+    @Test
+    public void testCreateMovieShow() throws Exception {
+        ObjectMapper om = new ObjectMapper();
+        CreateMovieShowInput movieShow = new CreateMovieShowInput("Superman", 87, "2017-12-31T18:09");
+        prepareDatabase();
+
+        mockMvc.perform(post("/movie_shows").contentType(MediaType.APPLICATION_JSON).content(om.writeValueAsString(movieShow)))
+                .andDo(print())
+                .andExpect(status().isCreated());
+    }
 
 }
